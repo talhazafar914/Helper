61fc71fde751065fce31ca7995546eb784e5d7ce
talhazafar914 <rajatalha9149@gmail.com>
2023-07-03 16:29:28 +0500

    Update NetworkCall.swift

--- NetworkCall.swift
+++ NetworkCall.swift
@@ -45,21 +45,21 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as? NSArray ?? [])
-                        print(dic)
-                        completion(dic,true)
-                    }
-                    else
-                    {
-                        completion([],true)
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([],false)
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as? NSArray ?? [])
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([],false)
             }
         }
     }
@@ -72,21 +72,21 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as? NSArray ?? [])
-                        print(dic)
-                        completion(dic,true)
-                    }
-                    else
-                    {
-                        completion([],true)
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([],false)
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as? NSArray ?? [])
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([],false)
             }
         }
     }
@@ -99,25 +99,25 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as! NSDictionary)
-                        print(dic)
-                        completion(dic,true)
-                    }
-                    else
-                    {
-                        completion([:],true)
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([:],false)
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
             }
         }
     }
-
+    
     
     /**This function takes URL and Parm and  its completion is Dictionary, Bool and Data*/
     func postRequest(Url:String,Parm:[String:Any], Header:HTTPHeaders ,completion: @escaping (NSDictionary,Bool,Data) -> ())
@@ -127,21 +127,21 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as? NSDictionary ?? [:])
-                        print(dic)
-                        completion(dic,true,response.data ?? Data())
-                    }
-                    else
-                    {
-                        completion([:],true,Data())
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([:],false,Data())
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as? NSDictionary ?? [:])
+                    print(dic)
+                    completion(dic,true,response.data ?? Data())
+                }
+                else
+                {
+                    completion([:],true,Data())
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false,Data())
             }
         }
     }
@@ -154,25 +154,25 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as! NSDictionary)
-                        print(dic)
-                        completion(dic,true)
-                    }
-                    else
-                    {
-                        completion([:],true)
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([:],false)
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
             }
         }
     }
-
+    
     /**Get request function, this function takes only URL  and  its completion is nsArray, Bool*/
     func getRequest(Url:String, Header:HTTPHeaders ,completion: @escaping (NSArray,Bool) -> ())
     {
@@ -181,24 +181,140 @@ class NetworkCall
             
             switch response.result
             {
-                case .success:
-                    if let JSON = response.value
-                    {
-                        print("JSON: \(JSON)")
-                        let dic = (JSON as! NSArray)
-                        print(dic)
-                        completion(dic,true)
-                    }
-                    else
-                    {
-                        completion([],true)
-                    }
-                case .failure(let error):
-                    print(error)
-                    completion([],false)
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSArray)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([],false)
             }
         }
     }
-
-
+    
+    /**Put Request with params and completion is NSDictionary and bool*/
+    func putRequest(Url:String,Parm:[String:Any], Header:HTTPHeaders ,completion: @escaping (NSDictionary,Bool) -> ())
+    {
+        
+        AF.request(URL(string: Url)!, method: .put, parameters: Parm, encoding: JSONEncoding.default, headers: Header).responseJSON
+        { (response:DataResponse) in
+            
+            switch response.result
+            {
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
+            }
+        }
+    }
+    
+    /**Put Request with url query completions is NSDictionary and Bool*/
+    func putRequest(Url:String, Header:HTTPHeaders ,completion: @escaping (NSDictionary,Bool) -> ())
+    {
+        guard let encodedUrl = Url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            print("Error encoding URL")
+            return
+        }
+        
+        AF.request(encodedUrl, method: .put, encoding: JSONEncoding.default, headers: Header).responseJSON
+        { (response:DataResponse) in
+            
+            switch response.result
+            {
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
+            }
+        }
+    }
+    
+    /**Delete Request with url and param and completion is NSDictionary and bool*/
+    func deleteRequest(Url:String,Parm:[String:Any], Header:HTTPHeaders ,completion: @escaping (NSDictionary,Bool) -> ())
+    {
+        
+        AF.request(URL(string: Url)!, method: .delete, parameters: Parm, encoding: JSONEncoding.default, headers: Header).responseJSON
+        { (response:DataResponse) in
+            
+            switch response.result
+            {
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
+            }
+        }
+    }
+    
+    /**Delete Request with Url completion is NSDictionary and Bool*/
+    func deleteRequest(Url:String, Header:HTTPHeaders ,completion: @escaping (NSDictionary,Bool) -> ())
+    {
+        
+        AF.request(URL(string: Url)!, method: .delete, parameters: nil, encoding: JSONEncoding.default, headers: Header).responseJSON
+        { (response:DataResponse) in
+            
+            switch response.result
+            {
+            case .success:
+                if let JSON = response.value
+                {
+                    print("JSON: \(JSON)")
+                    let dic = (JSON as! NSDictionary)
+                    print(dic)
+                    completion(dic,true)
+                }
+                else
+                {
+                    completion([:],true)
+                }
+            case .failure(let error):
+                print(error)
+                completion([:],false)
+            }
+        }
+    }
+    
+    
 }
